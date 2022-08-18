7a24216d54723d474e702babbdc5686b24a8e6f6
arohoza <107406993+arohoza@users.noreply.github.com>
2022-08-18 18:10:44 +0200

    fixes

--- js/task-08.js
+++ js/task-08.js
@@ -9,10 +9,32 @@ function handleSubmit(event) {
     elements: { email, password }
   } = event.currentTarget;
 
-  if (email.value === "" || password.value === "") {
-    alert("Please fill in all the fields!");
-  }
-  const data = { email: email.value, password:password.value };
+  if (email.value !== "" && password.value !== "") {
+    
+    const data = { email: email.value, password:password.value };
     console.log(data);
   event.currentTarget.reset();
+  }
+  else{alert("Please fill in all the fields!");}
 }
+
+
+
+// const formRef = document.querySelector('.login-form');
+// console.log(formRef);
+
+// formRef.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password }
+//   } = event.currentTarget;
+
+//   if (email.value === "" || password.value === "") {
+//     alert("Please fill in all the fields!");
+//   }
+//   const data = { email: email.value, password:password.value };
+//     console.log(data);
+//   event.currentTarget.reset();
+// }
